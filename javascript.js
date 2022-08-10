178d169f2d05d5b6de9d64f18f1575777c6a210a
jhow <lolrager123@gmail.com>
2022-08-10 14:58:10 -0700

    playRound() is working, added prompt 'rock, paper, or scissors' converts any letter in prompt to lower case.

--- javascript.js
+++ javascript.js
@@ -6,21 +6,35 @@ function getComputerChoice() {
   }
   
   
-  
-  
-  
-  function playRound(playerSelection, computerSelection) {
-      if (playerSelection === 'rock' && computerSelection === 'paper') {
-          return 'lose';
-      } 
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return 'You lose, paper beats rock';
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `You win, rock beats ${computerSelection}`;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return `You lose, scissors beats ${playerSelection}`;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return `You win, ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return `You lose, ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return `You win, ${playerSelection} beats ${computerSelection}`;
+    } else (playerSelection  === computerSelection) 
+        return `It's a tie, ${playerSelection} = ${computerSelection}`;
   }
   
   
-    
-    const playerSelection = 'rock';
+  
+    const playerSelection = prompt("What will you choose? 'Rock, Paper, Scissors'").toLowerCase()
     const computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
   
     console.log(computerSelection);
+
+
+function game() {
+
+}
     
     
