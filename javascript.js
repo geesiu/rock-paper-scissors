d708132f80b9d8c3cf63e3200ef14afb3592ad1a
jhow <lolrager123@gmail.com>
2022-10-26 16:57:41 -0700

    got play round working, now need to work on displaying loser or winner after 5 rounds

--- javascript.js
+++ javascript.js
@@ -7,59 +7,56 @@ function getComputerChoice() {
   
   
 
-function playRound() {
-    const playerSelection = prompt("What will you choose? 'Rock, Paper, Scissors'" ).toLowerCase()
-    const computerSelection = getComputerChoice()
-    if (playerSelection === "rock" && computerSelection === "paper") {
-        return 'You lose, paper beats rock';
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return `You win, rock beats ${computerSelection}`;
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return `You lose, scissors beats ${playerSelection}`;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return `You win, ${playerSelection} beats ${computerSelection}`;
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return `You lose, ${computerSelection} beats ${playerSelection}`;
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return `You win, ${playerSelection} beats ${computerSelection}`;
-    } else (playerSelection  === computerSelection) 
-        return `It's a tie, ${playerSelection} = ${computerSelection}`;
-        
-  }
+  
+  function playRound() {
+    let computerSelection = getComputerChoice()
+
+        if (this.innerHTML == "rock" && computerSelection == "scissors" || 
+            this.innerHTML == "paper" && computerSelection == "rock" ||
+            this.innerHTML == "scissors" && computerSelection == "paper") {
+          results.textContent = `You win, ${this.innerHTML} beats ${computerSelection}`;
+        } else if (this.innerHTML == computerSelection) {
+            results.textContent = `It's a tie ${this.innerHTML} equals ${computerSelection}`;
+        } else  {
+          results.textContent = `${this.innerHTML} loses against ${computerSelection}`;
+        };
+      }
+  
   
  
+
+  const rock = document.querySelector('.rock'); 
+  const paper = document.querySelector('.paper');
+  const scissors = document.querySelector('.scissors');
+    
+
+  const array = [rock, paper, scissors];
+    for (let i = 0; i < array.length; i++) {
+      array[i].addEventListener('click', playRound);    
+    }
   
+      
+   const results = document.querySelector('.results');
+  
+
    
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
-
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let roundResult = playRound()
-
-    for (let i = 0; i < 5; i++) {
-        playRound();
-
-        if (roundResult === 'You win, rock beats scissors' || roundResult === 'You win, paper beats rock' || roundResult === 'You win, scissors beats paper') {
-            ++playerScore;
-        } else if (roundResult === 'You lose, paper beats rock' || roundResult === 'You lose, scissors beats paper' || roundResult === 'You lose, rock beats scissors') {
-            ++computerScore;
-        }
-     }
-     
-     if (playerScore > computerScore)  {
-        return ('You win ' + playerScore + ' to ' + computerScore + '.');
-     }  else if (playerScore < computerScore) {
-        return ('You lose ' + computerScore + ' to ' + playerScore + '.');
-     }  else {
-        return 'It was a tie, both players have won the same number of rounds.';
-     }
-}  
-
-console.log(game());
-
-
 
 
 
